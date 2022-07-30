f8b72af086f6dbd52cac4a3ad1d3ddf45e5c585f
ejz93 <juarezestanislao93@gmail.com>
2022-07-30 16:08:19 -0400

    crossOrigins
    cambios

--- src/main/java/com/portfolio/ejz/Controller/EducacionController.java
+++ src/main/java/com/portfolio/ejz/Controller/EducacionController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://ejzfront.web.app")
 
 public class EducacionController {
     @Autowired
